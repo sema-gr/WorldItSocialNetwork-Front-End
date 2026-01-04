2e866611bde3e62e69b4045eee9e165d36dbdaa3
arman455 <snositel99@gmail.com>
2026-01-04 18:16:18 +0200

    fix: log API_BASE_URL for debugging purposes

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,2 @@
 export const API_BASE_URL = "https://worlditsocialnetwork-back-end-production.up.railway.app";
 // export const API_BASE_URL = "http://192.168.1.102:3000";
-console.log("API_BASE_URL:", API_BASE_URL);
